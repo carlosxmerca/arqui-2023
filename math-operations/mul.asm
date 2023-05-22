49348465597cea3bd66ee8d57e4f0e9b5087bd0a
Daniel Vladimir Solis Marroquin <62628411+DanielSolis00209020@users.noreply.github.com>
2023-05-21 23:13:30 -0600

    comments Mult. Solutions

--- math-operations/mul.asm
+++ math-operations/mul.asm
@@ -4,12 +4,12 @@
 
 ; multiplicación tamaño byte
         mov     AL, 5d
-        mov     BL, 5d
-        mul     BL    
+        mov     BL, 3d
+        mul     BL              ; AX: 000F
 
 ; multiplicación tamaño word
         mov     AX, 250h
-        mov     BX, 250h
-        mul     BX
+        mov     BX, 230h        ; Multiplicacion = 50F00h
+        mul     BX              ; AX: 0F00, DX: 0005
 
         int     20h
